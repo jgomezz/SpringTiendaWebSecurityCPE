25e0c5d9e26055da397958b125305587d1522106
jgomezz <jgomezz@gmail.com>
2023-12-17 12:10:03 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -1,20 +1,16 @@
 package pe.edu.tecsup.tienda.config;
 
 
-
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -57,5 +53,43 @@ public class SpringSecurityConfig {
 		auth.userDetailsService(userDetailsService);
 	}
 	//*/
+
+
+	// Autorization
+
+	//* 
+		@Bean
+		public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+			
+	       http
+	       // Configure authorizations
+	        .authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/" , "/**").permitAll()
+//		        .requestMatchers("/home/**").authenticated()
+//		        .requestMatchers("/admin/**").hasAnyAuthority("Administrador")	       
+	    	)
+	       // Change login
+		   	.formLogin((form) -> form
+		       	.loginPage("/login")  // la peticion para iniciar sesion
+		       	.loginProcessingUrl("/authenticate")
+		       	.defaultSuccessUrl("/")
+		       	.failureUrl("/login?error")
+		       	.usernameParameter("username").passwordParameter("password")
+		   	)
+		   	// Change logout
+		   	.logout((logout) -> logout
+					.logoutUrl("/logout")
+					.logoutSuccessUrl("/login")
+		    )
+		   	// Change csrf
+		   	.csrf( (csrf) -> csrf.disable());
+	      
+			return http.build();
+		}
+		//*/
+
+
+
+
 }
 
