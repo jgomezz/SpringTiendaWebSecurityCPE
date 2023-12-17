0d9bec245a50da32078b141f3159d0545ca5d31d
jgomezz <jgomezz@gmail.com>
2023-12-17 11:19:00 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -1,8 +1,9 @@
 package pe.edu.tecsup.tienda.config;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,16 +16,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
-
+	
+  
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	/*
+   public PasswordEncoder passwordEncoder() {
+       return new BCryptPasswordEncoder();
+   }   
+  
+	
+   /*
 	@Bean
 	public UserDetailsService userDetailsServiceBean() throws Exception {
 		
@@ -34,7 +38,6 @@ public class SpringSecurityConfig {
 					  .password(passwordEncoder().encode("user"))
 					  .roles("USER")
 					  .build());
-		
 		users.add(User.withUsername("admin")
 				      .password(passwordEncoder().encode("admin"))
 				      .roles("USER","ADMIN")
@@ -43,7 +46,6 @@ public class SpringSecurityConfig {
 		return new InMemoryUserDetailsManager(users);
 	}
 	//*/
-	
 	
 	//*
 	@Autowired
@@ -55,5 +57,5 @@ public class SpringSecurityConfig {
 		auth.userDetailsService(userDetailsService);
 	}
 	//*/
-	
 }
+
