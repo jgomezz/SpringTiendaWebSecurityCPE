e1d095818a697d2b0d5a114fb5cac17394f0aab2
jgomezz <jgomezz@gmail.com>
2023-12-17 09:49:47 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -0,0 +1,45 @@
+package pe.edu.tecsup.tienda.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	
+	//*
+	@Bean
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+		
+		List<UserDetails> users = new ArrayList<UserDetails>();
+		
+		users.add(User.withUsername("user")
+					  .password(passwordEncoder().encode("user"))
+					  .roles("USER")
+					  .build());
+		
+		users.add(User.withUsername("admin")
+				      .password(passwordEncoder().encode("admin"))
+				      .roles("USER","ADMIN")
+				      .build());
+		
+		return new InMemoryUserDetailsManager(users);
+	}
+	//*/
+}
